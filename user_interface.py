56e8e6dba8edcb07e6594146b7ebf40d2bf7ec17
Christian Abbott <cmagwoodva@gmail.com>
2023-12-15 10:40:07 -0500

    Maybe this will fix verification

--- user_interface.py
+++ user_interface.py
@@ -273,7 +273,7 @@ def fetch_message():
                 message_found = True
                 message = content
                 lines = message.split("\n")
-                sender = lines[2].replace("SENDER: ", "")
+                sender = lines[2].replace("SENDER: ", "").strip().replace("\n", "")
                 json_bundle = json.loads(lines[3])
                 break
         id -= 1
